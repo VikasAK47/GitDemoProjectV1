04a1d0cb19e644152e5a2d3d76094355f9bb4ad0
vikas <vikasaksmile@gmail.com>
2022-03-31 19:38:25 +0530

    First commit...

---
+++ src/demo/Testjava.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class Testjava {
+
+	public static void main(String[] args)
+	{
+		String hungry="yes";
+		
+		if(hungry == "yes")
+			System.out.println("eat something");
+		else {
+			System.out.println("do work");
+		}
+			
+
+	}
+
+}
